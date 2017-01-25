1447acade81243a88aec18cf9a073bb73bf887c7
heran <zhaoheran@google.com>
2017-01-25 17:27:10 +0800

    loop check IP

--- ConsoleCheckAzureIP/Program.cs
+++ ConsoleCheckAzureIP/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.IO;
 
 namespace ConsoleCheckAzureIP
 {
@@ -10,6 +12,28 @@ namespace ConsoleCheckAzureIP
     {
         static void Main(string[] args)
         {
+            string exitString = "";
+            // string checkIP = 
+            
+            while (exitString != "exit")
+            {
+                Console.WriteLine("\nPlease input the IP address: ");
+                string CheckIP = Console.ReadLine();
+                if (CheckIP == "exit")
+                    exitString = CheckIP;
+                else
+                {
+                    string url = "http://www.azurespeed.com/api/region?ipOrUrl=" + CheckIP;
+                    WebRequest request = WebRequest.Create(url);
+                    WebResponse response = request.GetResponse();
+                    StreamReader sr = new StreamReader(response.GetResponseStream());
+                    string html = sr.ReadToEnd();
+                    Console.WriteLine(html);
+                }
+            }
+
+            
+            // Console.ReadKey();
         }
     }
 }
